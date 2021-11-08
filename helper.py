298d40159572795b4c13fbc20e45fd503a9e5a89
Josh Daymude <joshdaymude@gmail.com>
2021-11-07 18:31:51 -0700

    Added plotting functions for manuscript figures

--- helper.py
+++ helper.py
@@ -7,8 +7,15 @@ helper: Helper functions for the GeorgiaLDUClosure analysis.
 """
 
 from itertools import product
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os.path as osp
 import pandas as pd
 import scipy.stats as ss
+
+# Set all plotting to use serif fonts for better matching with the manuscript.
+matplotlib.rcParams['font.family'] = 'serif'
 
 
 def show_df(df, show=True):
@@ -136,3 +143,55 @@ def two_by_two(df, ind1, ind2):
     print(ind2 + ', Closed: {:,.1f}'.format(closed[ind2].sum()))
     print(ind1 + ', Open: {:,.1f}'.format(open[ind1].sum()))
     print(ind2 + ', Open: {:,.1f}'.format(open[ind2].sum()))
+
+
+def plot_medians(labels, openvals, closedvals, pvals, barwidth=0.25, fmt='{}', \
+                 anno=''):
+    """
+    Plots a grouped bar graph for medians of measures by open and closed LDUs.
+    Labels the bars with their corresponding medians and the groups with their
+    Mann-Whitney U test p-values.
+
+    :param labels: a list of N string labels for the measures
+    :param openvals: a list of N median measure values for open LDUs
+    :param closedvals: a list of N median measure values for closed LDUs
+    :param pvals: a list of N p-values for the Mann-Whitney tests
+    :param barwidth: the width of the bars; should be in (0, 0.5)
+    :param fmt: a string format for the bar labels
+    :param anno: a string annotation for the plot filename
+    """
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=300, tight_layout=True)
+
+    # Plot the open/closed median bars.
+    x = np.arange(len(labels))
+    obars = ax.bar(x - 5*barwidth/8, openvals, barwidth, hatch='\\\\\\', \
+                   edgecolor='w', label='LDU Remained Open')
+    cbars = ax.bar(x + 5*barwidth/8, closedvals, barwidth, color='firebrick', \
+                   label='LDU Closed')
+
+    # Add the bar value labels.
+    ax.bar_label(obars, labels=[fmt.format(v) for v in openvals], padding=5)
+    ax.bar_label(cbars, labels=[fmt.format(v) for v in closedvals], padding=5)
+
+    # Get the largest y-value for spacing purposes.
+    ymax = max(openvals + closedvals)
+
+    # Add the median p-values, bolding and starring them if they're significant.
+    for i, pval in enumerate(pvals):
+        if pval < 0.05:
+            pstr = 'p = {:.3f}*'.format(pval)
+            pwgt = 'bold'
+        else:
+            pstr = 'p = {:.3f}'.format(pval)
+            pwgt = 'normal'
+        ax.text(x[i], 1.1*ymax, pstr, ha='center', weight=pwgt)
+
+    # Clean up the plot and set labels.
+    ax.spines.left.set_visible(False)
+    ax.spines.top.set_visible(False)
+    ax.spines.right.set_visible(False)
+    ax.set(xticks=x, xticklabels=labels, ylim=[0,1.3*ymax], yticks=[])
+    ax.legend()
+
+    # Save the figure.
+    fig.savefig(osp.join('figs', 'medians' + anno + '.png'))
