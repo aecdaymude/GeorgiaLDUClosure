d1aff07b8443f40c7a2786edfb9b4a588f9ee54c
Josh Daymude <joshdaymude@gmail.com>
2021-11-08 01:38:11 -0700

    Prepared data and analysis pipeline for publication
    ~ Updated README and requirements
    ~ Reorganized tables and figures like the manuscript
    + Aligned all output from statistics functions
    - Removed data preprocessing
    - Removed all unused derived columns and analyses
    - Removed means and 2-sample t-tests

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GeorgiaLDUClosure
+
+This repository contains the code used to analyze quantitative data in the paper "Labor and Delivery Unit Closures in Rural Georgia from 2012&ndash;2016 and the Impact on Black Women: A Mixed-Methods Investigation" by Anna E. C. Daymude, Joshua J. Daymude, and Roger Rochat.
+
+The main analysis code, outputs, and plots with narrative descriptions can be found in the [`analysis.ipynb`](https://github.com/aecdaymude/GeorgiaLDUClosure/blob/main/analysis.ipynb) Jupyter notebook.
+Supporting analysis functions are contained in [`helper.py`](https://github.com/aecdaymude/GeorgiaLDUClosure/blob/main/helper.py).
--- helper.py
+++ helper.py
@@ -30,39 +30,11 @@ def show_df(df, show=True):
         display(df)
 
 
-def basic_stats(df, measure, agg='sum'):
-    """
-    Reports the total, sizes, and proportions of the measure by closure status.
-
-    :param df: a pandas DataFrame containing a 'Closed 2012-2016' column
-    :param measure: a string column name of numeric data to report stats on
-    :param agg: a string indicating how to aggregate the measure and groups
-    """
-    # Group by closure status.
-    gb = df.groupby('Closed 2012-2016', as_index=False)
-
-    # Calculate basic stats.
-    if agg == 'sum':
-        total = df[measure].sum()
-        open = gb.get_group(0)[measure].sum()
-        closed = gb.get_group(1)[measure].sum()
-    elif agg == 'count':
-        total = len(df[measure])
-        open = len(gb.get_group(0))
-        closed = len(gb.get_group(1))
-
-    # Report results.
-    print(measure + '\n' + ''.join(['-' for i in range(len(measure))]))
-    print('Total:\t\t{:,.3f}'.format(total))
-    print('Open:\t\t{:,.3f}\t({:,.3f}%)'.format(open, 100 * open / total))
-    print('Closed:\t\t{:,.3f}\t({:,.3f}%)'.format(closed, 100 * closed / total))
-
-
 def count_stats(df, measure):
     """
-    Reports the total, means, medians, minimums, and maximums of the given Count
-    measure by closure status and compares their distributions using an
-    Independent 2-Sample t-Test and a Mann-Whitney U Rank Test for significance.
+    Reports the total, medians, minimums, and maximums of the given Count
+    measure by closure status and compares their distributions using a
+    Mann-Whitney U test for significance.
 
     :param df: a pandas DataFrame containing a 'Closed 2012-2016' column
     :param measure: a string column name of numeric data to report stats on
@@ -72,28 +44,25 @@ def count_stats(df, measure):
     open = gb.get_group(0)[measure]
     closed = gb.get_group(1)[measure]
 
-    # Perform statistical tests.
-    _, pval_t = ss.ttest_ind(open.dropna(), closed.dropna())
-    _, pval_mw = ss.mannwhitneyu(open.dropna(), closed.dropna(), method='exact')
+    # Perform statistical test.
+    _, pval = ss.mannwhitneyu(open.dropna(), closed.dropna(), method='exact')
 
     # Report results.
     print(measure + '\n' + ''.join(['-' for i in range(len(measure))]))
-    print('Total:\t\t{:,.3f}'.format(df[measure].sum()))
-    print('Open:\t\tmean={:,.3f}\tmedian={:,.3f}\t({:,.3f} - {:,.3f})'\
-          .format(open.mean(), open.median(), open.min(), open.max()))
-    print('Closed:\t\tmean={:,.3f}\tmedian={:,.3f}\t({:,.3f} - {:,.3f})'\
-          .format(closed.mean(), closed.median(), closed.min(), closed.max()))
-    print('2-Samp t-Test:\tpval={:.9f}'.format(pval_t))
-    print('Mann-Whit Test:\tpval={:.9f}'.format(pval_mw))
+    print("{:<12}{:,.3f}".format('Total:', df[measure].sum()))
+    print("{:<12}median={:<12,.3f}({:,.3f} - {:,.3f})"\
+          .format('Open:', open.median(), open.min(), open.max()))
+    print("{:<12}median={:<12,.3f}({:,.3f} - {:,.3f})"\
+          .format('Closed:', closed.median(), closed.min(), closed.max()))
+    print("{:<12}pval={:.9f}".format('Mann-Whit:', pval))
 
 
 def proportion_stats(df, numer_measure, denom_measure):
     """
     Similar to the above function for Count measures but takes two columns, a
     numerator and a denominator, which together form a Proportion measure.
-    Reports the total of the numerator column and the means, medians, minimums,
-    and maximums of the Proportion, using an Independent 2-Sample t-Test and a
-    Mann-Whitney U Rank Test for significance.
+    Reports the total of the numerator column and the medians, minimums, and
+    maximums of the Proportion, using a Mann-Whitney U test for significance.
 
     :param df: a pandas DataFrame containing a 'Closed 2012-2016' column
     :param numer_measure: a string column name of numeric data to treat as the
@@ -103,25 +72,23 @@ def proportion_stats(df, numer_measure, denom_measure):
     """
     # Create a new DataFrame with the additional % column and group by closure.
     prop_df = df.copy()
-    prop_df['%'] = 100 * prop_df[numer_measure] / prop_df[denom_measure]
+    prop_df['%'] = prop_df[numer_measure] / prop_df[denom_measure]
     gb = prop_df.groupby('Closed 2012-2016', as_index=False)
     open = gb.get_group(0)['%']
     closed = gb.get_group(1)['%']
 
-    # Perform statistical tests.
-    _, pval_t = ss.ttest_ind(open.dropna(), closed.dropna())
-    _, pval_mw = ss.mannwhitneyu(open.dropna(), closed.dropna(), method='exact')
+    # Perform statistical test.
+    _, pval = ss.mannwhitneyu(open.dropna(), closed.dropna(), method='exact')
 
     # Report results.
     print('% ' + numer_measure)
     print(''.join(['-' for i in range(len(numer_measure) + 2)]))
-    print('Total:\t\t{:,.3f}'.format(prop_df[numer_measure].sum()))
-    print('Open:\t\tmean={:,.3f}%\tmedian={:,.3f}%\t({:,.3f}% - {:,.3f}%)'\
-          .format(open.mean(), open.median(), open.min(), open.max()))
-    print('Closed:\t\tmean={:,.3f}%\tmedian={:,.3f}%\t({:,.3f}% - {:,.3f}%)'\
-          .format(closed.mean(), closed.median(), closed.min(), closed.max()))
-    print('2-Samp t-Test:\tpval={:.9f}'.format(pval_t))
-    print('Mann-Whit Test:\tpval={:.9f}'.format(pval_mw))
+    print("{:<12}{:,.3f}".format('Total:', prop_df[numer_measure].sum()))
+    print("{:<12}median={:<12,.3%}({:,.3%} - {:,.3%})"\
+          .format('Open:', open.median(), open.min(), open.max()))
+    print("{:<12}median={:<12,.3%}({:,.3%} - {:,.3%})"\
+          .format('Closed:', closed.median(), closed.min(), closed.max()))
+    print("{:<12}pval={:.9f}".format('Mann-Whit:', pval))
 
 
 def two_by_two(df, ind1, ind2):
@@ -139,10 +106,11 @@ def two_by_two(df, ind1, ind2):
     open, closed = gb.get_group(0), gb.get_group(1)
 
     # Print the 2x2 contingency table data.
-    print(ind1 + ', Closed: {:,.1f}'.format(closed[ind1].sum()))
-    print(ind2 + ', Closed: {:,.1f}'.format(closed[ind2].sum()))
-    print(ind1 + ', Open: {:,.1f}'.format(open[ind1].sum()))
-    print(ind2 + ', Open: {:,.1f}'.format(open[ind2].sum()))
+    k = max(len(ind1), len(ind2)) + len(', Closed:') + 2
+    print("{:<{}}{:,.2f}".format(ind1+', Closed:', k, closed[ind1].sum()))
+    print("{:<{}}{:,.2f}".format(ind2+', Closed:', k, closed[ind2].sum()))
+    print("{:<{}}{:,.2f}".format(ind1+', Open:', k, open[ind1].sum()))
+    print("{:<{}}{:,.2f}".format(ind2+', Open:', k, open[ind2].sum()))
 
 
 def plot_medians(labels, openvals, closedvals, pvals, barwidth=0.25, fmt='{}', \
